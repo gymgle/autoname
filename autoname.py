a732a05291faad45ff92e8fa58c81013b286e802
Yaming Gong <ymgongcn@gmail.com>
2023-04-08 15:19:39 +0800

    feat: refact argument

--- autoname.py
+++ autoname.py
@@ -33,26 +33,23 @@ def print_version():
 if __name__ == '__main__':
     # Args analysis
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--auto', action='store_true', default=False,
-                        help='auto rename files of given path')
     parser.add_argument('-p', '--path', type=str, default='',
                         help='path to files needed rename')
     parser.add_argument('-f', '--format', type=str, default='%Y-%m-%d %H:%M:%S',
                         help='new name format in python datetime')
     parser.add_argument('-t', '--datetime', type=str, default='original',
                         help='which datetime should be choosen for the new name: original, create, modify, filename')
-    parser.add_argument('-d', '--android', action='store_true', default=False,
-                        help='auto rename only for Android DCIM files')
-    parser.add_argument('-w', '--wechat', action='store_true', default=False,
-                        help='auto rename only for Wechat files')
+    parser.add_argument('-oi', '--only-image', action='store_true', default=False,
+                        help='rename only for image type')
+    parser.add_argument('-ov', '--only-video', action='store_true', default=False,
+                        help='rename only for video type')
     parser.add_argument('-v', '--version', action='store_true', help='show version', default=False)
     args = vars(parser.parse_args())
-    auto = args.get('auto', False)
     file_path = args.get('path', '')
     file_format = args.get('format', '')
     file_datetime = args.get('datetime', '')
-    android = args.get('android', False)
-    wechat = args.get('wechat', False)
+    only_image = args.get('only-image', False)
+    only_video = args.get('only-video', False)
     version = args.get('version', False)
 
     # Show version
